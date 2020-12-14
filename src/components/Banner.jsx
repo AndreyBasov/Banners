0b98cf38430ad168b93b12e990195fd268bc1ad4
Basov Andrey <andreybasov0@gmail.com>
2020-12-14 21:35:39 +0300

    loads faster

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,58 +1,64 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
-import GetAppIcon from '@material-ui/icons/GetApp';
-import {Modal} from 'react-bootstrap';
+import GetAppIcon from "@material-ui/icons/GetApp";
+import { Modal } from "react-bootstrap";
 
 const Banner = (props) => {
+  let [loading, setLoading] = useState(true);
 
   const handleDeleteClick = (event) => {
     props.onDelete(props.id);
-    event.stopPropagation()
-  }
+    event.stopPropagation();
+  };
 
   const handleDownloadClick = (event) => {
     setExpanded2(true);
     event.stopPropagation();
-  }
-  
+  };
 
   const openStories = (event) => {
     props.onBannerClick(props.id);
     event.stopPropagation();
-  }
+  };
 
   const handleDownload2 = (event) => {
-      const el = document.createElement('textarea');
-      el.value = props.url;
-      document.body.appendChild(el);
-      el.select();
-      document.execCommand('copy');
-      document.body.removeChild(el);
-  }
+    const el = document.createElement("textarea");
+    el.value = props.url;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  };
 
-  const handleDownload3 = event => {
-    const el = document.createElement('textarea');
+  const handleDownload3 = (event) => {
+    const el = document.createElement("textarea");
     el.value = JSON.stringify({
       title: props.title,
       content: props.content,
-      url: props.url
+      url: props.url,
     });
     document.body.appendChild(el);
     el.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     document.body.removeChild(el);
-  }
+  };
 
   let [isExpanded2, setExpanded2] = useState(false);
   const closeModal = (event) => {
     setExpanded2(false);
-  }
- 
+  };
+
   return (
     <div>
       <div onClick={openStories} className="note">
-        <img src={props.url} alt={props.id}></img> 
-        <h1 style = {{color: props.color}}>{props.title.substring(0, 30)}</h1>
+        <img
+          style={loading ? { display: "none" } : {}}
+          src={props.src}
+          onLoad={() => setLoading({ loading: false })}
+          alt="banner"
+        />
+        <img src={props.url} alt={props.id}></img>
+        <h1 style={{ color: props.color }}>{props.title.substring(0, 30)}</h1>
         <button className="delete-button" onClick={handleDeleteClick}>
           <DeleteIcon />
         </button>
@@ -60,13 +66,17 @@ const Banner = (props) => {
           <GetAppIcon />
         </button>
       </div>
-      <Modal centered= {true} show={isExpanded2} onHide={closeModal}>
-          <Modal.Body>
+      <Modal centered={true} show={isExpanded2} onHide={closeModal}>
+        <Modal.Body>
           <div className="download-field">
             <ul>
               <li>
                 <p>Скачать как .png</p>
-                <a href={props.url} download={props.id + ".png"}><button><GetAppIcon /></button></a>
+                <a href={props.url} download={props.id + ".png"}>
+                  <button>
+                    <GetAppIcon />
+                  </button>
+                </a>
               </li>
             </ul>
             <ul>
@@ -82,14 +92,14 @@ const Banner = (props) => {
                 <p>Скопировать json</p>
                 <button onClick={handleDownload3}>
                   <GetAppIcon />
-               </button>
+                </button>
               </li>
             </ul>
-            </div>
-          </Modal.Body>
-        </Modal>
-      </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
   );
-}
+};
 
 export default Banner;
